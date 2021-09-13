9e81d1da9f0d7043ffa714394cf97ee4d56da8ec
Yukhyeonjin <37572364+Yukhyeonjin@users.noreply.github.com>
2021-09-13 19:48:51 +0900

    [2021.09.13 YHJ] 1. Bronze_2562 2. Bronze_2577

---
+++ src/com/solved/yuk/bronze/Bronze_2562.java
@@ -0,0 +1,21 @@
+package com.solved.yuk.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Bronze_2562 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int max = 0;
+        int order = 0;
+        for(int i = 0; i < 9; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if(num > max) {
+                max = num;
+                order = i+1;
+            }
+        }
+        System.out.printf("%d\n%d",max, order);
+    }
+}
