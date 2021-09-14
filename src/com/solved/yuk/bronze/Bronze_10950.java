3983280b978527eb73aea4159a4f0a0647518242
Yukhyeonjin <37572364+Yukhyeonjin@users.noreply.github.com>
2021-09-14 20:55:04 +0900

    [2021.09.13 YHJ] 1. Bronze_10950

---
+++ src/com/solved/yuk/bronze/Bronze_10950.java
@@ -0,0 +1,20 @@
+package com.solved.yuk.bronze;
+
+import java.io.*;
+
+public class Bronze_10950 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        for(int i = 0; i < N; i ++) {
+            String[] input = br.readLine().split(" ");
+            int firstNum = Integer.parseInt(input[0]), secondNum = Integer.parseInt(input[1]);
+            bw.write(firstNum + secondNum + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
