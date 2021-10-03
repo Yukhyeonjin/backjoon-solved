f1af35ebfde005b0c4fe261fe0449480595fc801
Yukhyeonjin <37572364+Yukhyeonjin@users.noreply.github.com>
2021-10-03 15:27:48 +0900

    [2021.10.03 YHJ] 1. Bronze_7287

---
+++ src/com/solved/yuk/etc/bronze/Bronze_7287.java
@@ -0,0 +1,16 @@
+package com.solved.yuk.etc.bronze;
+
+import java.io.*;
+
+public class Bronze_7287 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("63\nyukhyunjin");
+
+        bw.flush();
+        bw.close();
+
+    }
+}
