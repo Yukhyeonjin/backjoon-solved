56c2b1841879f29a541785fb66130f21dd9dafb2
Yukhyeonjin <37572364+Yukhyeonjin@users.noreply.github.com>
2021-10-14 22:11:08 +0900

    [2021.10.14 YHJ] 1. Bronze_10757

---
+++ src/com/solved/yuk/etc/bronze/Bronze_10757.java
@@ -0,0 +1,20 @@
+package com.solved.yuk.etc.bronze;
+
+import java.io.*;
+import java.math.BigInteger;
+
+public class Bronze_10757 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] input = br.readLine().split(" ");
+        BigInteger A = new BigInteger(input[0]),
+                   B = new BigInteger(input[1]);
+        bw.write(A.add(B).toString());
+
+        bw.flush();
+        bw.close();
+
+    }
+}
