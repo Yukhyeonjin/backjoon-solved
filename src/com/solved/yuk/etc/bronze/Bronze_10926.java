786d8e6ea181e4b5a27080cacde6fc8102c9fb41
Yukhyeonjin <37572364+Yukhyeonjin@users.noreply.github.com>
2021-10-23 14:14:39 +0900

    [2021.10.23 YHJ] 1. Bronze_10926

---
+++ src/com/solved/yuk/etc/bronze/Bronze_10926.java
@@ -0,0 +1,18 @@
+package com.solved.yuk.etc.bronze;
+
+import java.io.*;
+import java.math.BigInteger;
+
+public class Bronze_10926 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String input = br.readLine();
+        bw.write(input+"??!");
+
+        bw.flush();
+        bw.close();
+
+    }
+}
