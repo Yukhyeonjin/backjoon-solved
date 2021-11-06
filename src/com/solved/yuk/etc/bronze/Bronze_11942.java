ecbac43271da8e83fcf077ea2ba0709d20bda086
Yukhyeonjin <37572364+Yukhyeonjin@users.noreply.github.com>
2021-11-06 10:16:57 +0900

    [2021.11.06 YHJ] 1. Bronze_11942

---
+++ src/com/solved/yuk/etc/bronze/Bronze_11942.java
@@ -0,0 +1,15 @@
+package com.solved.yuk.etc.bronze;
+
+import java.io.*;
+
+public class Bronze_11942 {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("고려대학교");
+
+        bw.flush();
+        bw.close();
+
+    }
+}
