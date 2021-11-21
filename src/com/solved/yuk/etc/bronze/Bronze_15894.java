c258413fa73ba3c91a21e21865bf917383ef7f1c
Yukhyeonjin <37572364+Yukhyeonjin@users.noreply.github.com>
2021-11-21 17:56:34 +0900

    [2021.11.21 YHJ] 1. Bronze_15894

---
+++ src/com/solved/yuk/etc/bronze/Bronze_15894.java
@@ -0,0 +1,11 @@
+package com.solved.yuk.etc.bronze;
+
+import java.math.BigInteger;
+import java.util.*;
+
+public class Bronze_15894 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextBigInteger().multiply(BigInteger.valueOf(4)));
+    }
+}
