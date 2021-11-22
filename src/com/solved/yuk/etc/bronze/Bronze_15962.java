412542509b37c2ca878da8207c15135253847649
Yukhyeonjin <37572364+Yukhyeonjin@users.noreply.github.com>
2021-11-22 23:12:33 +0900

    [2021.11.22 YHJ] 1. Bronze_15962

---
+++ src/com/solved/yuk/etc/bronze/Bronze_15962.java
@@ -0,0 +1,14 @@
+package com.solved.yuk.etc.bronze;
+
+import java.io.*;
+
+public class Bronze_15962 {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("파이팅!!");
+
+        bw.flush();
+        bw.close();
+    }
+}
