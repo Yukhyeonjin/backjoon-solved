18758f3bd75f6158fe500df00916472992253828
Yukhyeonjin <37572364+Yukhyeonjin@users.noreply.github.com>
2021-12-04 18:47:24 +0900

    [2021.12.04 YHJ] 1. Bronze_17496

---
+++ src/com/solved/yuk/etc/bronze/Bronze_17496.java
@@ -0,0 +1,23 @@
+package com.solved.yuk.etc.bronze;
+
+import java.util.Scanner;
+
+public class Bronze_17496 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int T = sc.nextInt();
+        int C = sc.nextInt();
+        int P = sc.nextInt();
+
+        int cnt = 0;
+        int day = 1+T;
+
+        while(day<=N) {
+            cnt+=C;
+            day+=T;
+        }
+        System.out.println(cnt*P);
+    }
+}
