18eacb1e71f49fd13d31a218861a5060f684fb90
Yukhyeonjin <37572364+Yukhyeonjin@users.noreply.github.com>
2021-12-10 14:58:47 +0900

    [2021.12.10 YHJ] 1. Bronze_16394

---
+++ src/com/solved/yuk/etc/bronze/Bronze_16394.java
@@ -0,0 +1,11 @@
+package com.solved.yuk.etc.bronze;
+
+import java.util.Scanner;
+
+public class Bronze_16394 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(n - 1946);
+    }
+}
