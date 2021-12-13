43bfaadc6d00ccacfe5e8a4ca53850314224571b
Yukhyeonjin <37572364+Yukhyeonjin@users.noreply.github.com>
2021-12-13 22:06:56 +0900

    [2021.12.13 YHJ] 1. Bronze_1212 update

--- src/com/solved/yuk/etc/bronze/Bronze_1212.java
+++ src/com/solved/yuk/etc/bronze/Bronze_1212.java
@@ -6,8 +6,6 @@ public class Bronze_1212 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.next();
-        int sum=0;
-        int len=s.length();
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < s.length(); i++) {
             String a = Integer.toBinaryString(s.charAt(i)-'0');
