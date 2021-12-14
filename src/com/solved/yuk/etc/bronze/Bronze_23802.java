25bd389ee8ce3c70cf457934a8ca3a6453261b23
Yukhyeonjin <37572364+Yukhyeonjin@users.noreply.github.com>
2021-12-14 21:08:57 +0900

    [2021.12.14 YHJ] 1. Bronze_23802

---
+++ src/com/solved/yuk/etc/bronze/Bronze_23802.java
@@ -0,0 +1,27 @@
+package com.solved.yuk.etc.bronze;
+
+import java.io.*;
+
+public class Bronze_23802 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        for(int i = 0; i < N; i ++){
+            for(int j = 0; j < N*5; j ++){
+                bw.write("@");
+            }
+            bw.newLine();
+        }
+        for(int i = 0; i < (N*5)-N; i ++){
+            for(int j = 0; j <N; j ++){
+                bw.write("@");
+            }
+            bw.newLine();
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
