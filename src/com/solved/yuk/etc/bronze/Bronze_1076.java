06465e38f832f169b856343918a326e5ce4d9dd7
Yukhyeonjin <37572364+Yukhyeonjin@users.noreply.github.com>
2021-12-18 21:28:07 +0900

    [2021.12.18 YHJ] 1. Bronze_1076

---
+++ src/com/solved/yuk/etc/bronze/Bronze_1076.java
@@ -0,0 +1,37 @@
+package com.solved.yuk.etc.bronze;
+
+import java.io.*;
+import java.util.HashMap;
+
+public class Bronze_1076 {
+    static HashMap<String, String> colors;
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        colors = new HashMap<String, String>();
+        getColors();
+
+        // 첫 번째 색
+        String firstColor = br.readLine();
+        String secondColor = br.readLine();
+        String lastColor = br.readLine();
+        String resistanceStr = colors.get(firstColor).split("_")[0] + colors.get(secondColor).split("_")[0];
+        long resist = Long.valueOf(resistanceStr) * Long.valueOf(colors.get(lastColor).split("_")[1]);
+
+        System.out.println(resist);
+    }
+
+    static void getColors()	{
+        colors.put("black", "0_1");
+        colors.put("brown", "1_10");
+        colors.put("red", "2_100");
+        colors.put("orange", "3_1000");
+        colors.put("yellow", "4_10000");
+        colors.put("green", "5_100000");
+        colors.put("blue", "6_1000000");
+        colors.put("violet", "7_10000000");
+        colors.put("grey", "8_100000000");
+        colors.put("white", "9_1000000000");
+    }
+}
