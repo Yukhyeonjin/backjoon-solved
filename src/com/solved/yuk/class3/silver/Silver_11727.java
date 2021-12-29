cdcf320855914d0420de4764e201a1f466db0b04
육현진 <37572364+Yukhyeonjin@users.noreply.github.com>
2021-12-29 12:57:25 +0900

    [2021.12.29 YHJ]
    1. Silver_11727

---
+++ src/com/solved/yuk/class3/silver/Silver_11727.java
@@ -0,0 +1,28 @@
+package com.solved.yuk.class3.silver;
+
+import java.io.*;
+
+public class Silver_11727 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        // class3
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[n+2];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 3;
+
+        bw.write(dp(dp,n)+"");
+
+        bw.flush();
+        bw.close();
+    }
+
+    public static int dp(int[] dp, int n){
+        for(int i = 3; i <= n; i++){
+            dp[i] = (dp[i-1]+dp[i-2]*2) % 10007;
+        }
+        return dp[n];
+    }
+}
