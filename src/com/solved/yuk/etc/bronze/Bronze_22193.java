05b209654f7a3e420e7cedad1c4075b3bb975896
육현진 <37572364+Yukhyeonjin@users.noreply.github.com>
2022-01-15 15:08:54 +0900

    [2022.01.15 YHJ]
    1. Bronze_22193

---
+++ src/com/solved/yuk/etc/bronze/Bronze_22193.java
@@ -0,0 +1,19 @@
+package com.solved.yuk.etc.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Bronze_22193 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		String s = bf.readLine();
+		BigInteger a = new BigInteger(bf.readLine());
+		BigInteger b = new BigInteger(bf.readLine());
+		System.out.println(a.multiply(b));
+
+	}
+
+}
