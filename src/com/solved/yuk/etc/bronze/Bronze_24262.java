9072da29e49cb669cdfc3a5506932e9307ca1ce8
Yukhyeonjin <37572364+Yukhyeonjin@users.noreply.github.com>
2022-01-18 23:25:49 +0900

    [2022.01.18 YHJ] 1. Bronze_24262

---
+++ src/com/solved/yuk/etc/bronze/Bronze_24262.java
@@ -0,0 +1,17 @@
+package com.solved.yuk.etc.bronze;
+
+import java.io.*;
+
+public class Bronze_24262 {
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("1\n0");
+
+        bw.flush();
+        bw.close();
+    }
+
+}
