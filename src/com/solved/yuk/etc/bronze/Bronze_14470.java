11ae628ce78111589a02dac7f03663ac7df14ff5
육현진 <37572364+Yukhyeonjin@users.noreply.github.com>
2022-01-23 14:19:30 +0900

    [2022.01.23 YHJ]
    1. Bronze_14470

---
+++ src/com/solved/yuk/etc/bronze/Bronze_14470.java
@@ -0,0 +1,27 @@
+package com.solved.yuk.etc.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Bronze_14470 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int c = Integer.parseInt(br.readLine());
+        int d = Integer.parseInt(br.readLine());
+        int e = Integer.parseInt(br.readLine());
+        System.out.println(go(a, b, c, d, e));
+    } static int go(int now, int target, int c, int d, int e) {
+        if (now == target) {
+            return 0;
+        } else if (now < 0) {
+            return go(now + 1, target, c, d, e) + c;
+        } else if(now==0){
+            return go(now + 1, target, c, d, e) + d + e;
+        } else{ return go(now + 1, target, c, d, e) + e; 
+              } 
+    } 
+}
+
