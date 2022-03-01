fd7e18aac1f4097e7398cc53d64bce34619f1083
Yukhyeonjin <37572364+Yukhyeonjin@users.noreply.github.com>
2022-03-01 22:00:39 +0900

    [2022.03.01 YHJ] 1. Bronze_11021 2. Bronze_11022

---
+++ src/com/solved/yuk/etc/bronze/Bronze_11021.java
@@ -0,0 +1,20 @@
+package com.solved.yuk.etc.bronze;
+
+import java.io.*;
+
+public class Bronze_11021 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine()); // 테스트케이스
+        String input[];
+        for(int i = 0; i < T; i ++){
+            input = br.readLine().split(" ");
+            bw.write("Case #"+(i+1)+": "+(Integer.parseInt(input[0])+Integer.parseInt(input[1]))+"\n");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
