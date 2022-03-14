e3996c527628f54314779323ae1ab1d069a63cfe
Yukhyeonjin <37572364+Yukhyeonjin@users.noreply.github.com>
2022-03-14 19:21:11 +0900

    [2022.03.14 YHJ] 1. Bronze_10956

---
+++ src/com/solved/yuk/etc/bronze/Bronze_10956.java
@@ -0,0 +1,21 @@
+package com.solved.yuk.etc.bronze;
+
+import java.io.*;
+
+public class Bronze_10956 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < T; i++){
+            String[] input = br.readLine().split(",");
+            bw.write(Integer.parseInt(input[0])+Integer.parseInt(input[1])+"\n");
+        }
+
+        bw.flush();
+        bw.close();
+
+    }
+}
