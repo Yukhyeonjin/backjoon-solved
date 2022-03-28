71f7917ebd820dd2bea600b7e55f41b51954cf3c
육현진 <37572364+Yukhyeonjin@users.noreply.github.com>
2022-03-28 14:21:23 +0900

    [2022.03.28 YHJ]
    1. Bronze_2743

---
+++ src/com/solved/yuk/etc/bronze/Bronze_2743.java
@@ -0,0 +1,15 @@
+package com.solved.yuk.etc.bronze;
+
+import java.io.*;
+
+public class Bronze_2743 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write(br.readLine().length()+"");
+
+        bw.flush();
+        bw.close();
+    }
+}
