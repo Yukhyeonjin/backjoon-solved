d38c77b90622e1a831344ec8e84a9ddfcf9051de
육현진 <37572364+Yukhyeonjin@users.noreply.github.com>
2022-04-02 17:02:26 +0900

    [2022.04.02 YHJ]
    1. Bronze_14924

---
+++ src/com/solved/yuk/etc/bronze/Bronze_14924.java
@@ -0,0 +1,11 @@
+package com.solved.yuk.etc.bronze;
+
+import java.util.Scanner;
+
+public class Bronze_14924{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int s = sc.nextInt(), t = sc.nextInt(), d = sc.nextInt();
+        System.out.println(d/(2*s)*t);
+    }
+}
