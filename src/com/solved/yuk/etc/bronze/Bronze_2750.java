ceee275fd81af1946ab5655dd43592abf43f3fe0
Yukhyeonjin <37572364+Yukhyeonjin@users.noreply.github.com>
2022-04-24 20:56:59 +0900

    [2022.04.24 YHJ] 1. Bronze_2750

---
+++ src/com/solved/yuk/etc/bronze/Bronze_2750.java
@@ -0,0 +1,24 @@
+package com.solved.yuk.etc.bronze;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class Bronze_2750 {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine()), arr[] = new int[N];
+        for(int i = 0; i < N; i ++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+        for(int i = 0; i < N; i ++){
+            bw.write(arr[i]+"\n");
+        }
+
+
+        bw.flush();
+        bw.close();
+    }
+}
