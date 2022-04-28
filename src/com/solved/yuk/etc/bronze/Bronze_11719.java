19fd1a529928456d7f3b3a46b780d60e50a7258a
육현진 <37572364+Yukhyeonjin@users.noreply.github.com>
2022-04-28 16:36:12 +0900

    [2022.04.28 YHJ]
    1. Bronze_11719

---
+++ src/com/solved/yuk/etc/bronze/Bronze_11719.java
@@ -0,0 +1,17 @@
+package com.solved.yuk.etc.bronze;
+
+import java.io.*;
+
+public class Bronze_11719 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String input;
+        while ((input = br.readLine()) != null){
+            bw.write(input+"\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
