5450473378c2c00cdf7025c15b242d96da240063
육현진 <37572364+Yukhyeonjin@users.noreply.github.com>
2022-05-04 17:26:18 +0900

    [2022.05.04 YHJ]
    1. Bronze_8871

---
+++ src/com/solved/yuk/etc/bronze/Bronze_8871.java
@@ -0,0 +1,12 @@
+package com.solved.yuk.etc.bronze;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Bronze_8871 {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.printf("%d %d", (n+1)*2, (n+1)*3);
+    }
+}
