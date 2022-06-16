554b3ea383f22c2eba81702a46b215a9a759aff5
육현진 <37572364+Yukhyeonjin@users.noreply.github.com>
2022-06-16 17:29:31 +0900

    [2022.06.16 YHJ]
    1. Bronze_10992

---
+++ src/com/solved/yuk/etc/bronze/Bronze_10992.java
@@ -0,0 +1,31 @@
+package com.solved.yuk.etc.bronze;
+
+import java.util.Scanner;
+
+public class Bronze_10992 {
+    public static void main(String args[]) {
+
+      Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        
+        for(int i=1; i<n; i++) {
+        	System.out.printf(" ");
+        }System.out.println("*");
+        
+        for(int i=2; i<=n-1; i++) {
+        	for(int j=1; j<=n-i; j++) {
+        		System.out.printf(" ");
+        	} System.out.printf("*");
+        	for(int j=1; j<=(i-1)*2-1; j++) {
+        		System.out.printf(" ");
+        	}System.out.println("*");
+        	
+        }
+        
+        if(n!=1) {
+			for(int z=1; z<2*n; z++) {
+				System.out.printf("*");
+			}
+		}
+     }
+}
