60d62ad982a8f71ee30423803e18cb24a3efc57d
Yukhyeonjin <37572364+Yukhyeonjin@users.noreply.github.com>
2022-07-02 15:48:42 +0900

    [2022.07.02 YHJ] 1. Bronze_6810

---
+++ src/com/solved/yuk/etc/bronze/Bronze_6810.java
@@ -0,0 +1,35 @@
+package com.solved.yuk.etc.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Bronze_6810 {
+
+    private static String ISBN = "9780921418";
+    private static int mod13 = 3;
+
+    public static void main(String[] args) throws IOException {
+        final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int sum = 0;
+        for (String eachDigit : ISBN.split("")) {
+            sum += Integer.parseInt(eachDigit) * getMod123();
+        }
+
+        for (int i = 0; i < 3; i++) {
+            sum += Integer.parseInt(br.readLine()) * getMod123();
+        }
+
+        System.out.println("The 1-3-sum is " + sum);
+    }
+
+    private static int getMod123() {
+        if (mod13 == 1) {
+            mod13 = 3;
+        } else {
+            mod13 = 1;
+        }
+        return mod13;
+    }
+}
