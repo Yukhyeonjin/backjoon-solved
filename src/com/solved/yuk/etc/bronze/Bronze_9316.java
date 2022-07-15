8e6e174f3360be9525e0e28bcb329b374e70712c
Yukhyeonjin <37572364+Yukhyeonjin@users.noreply.github.com>
2022-07-16 07:43:42 +0900

    [2022.07.16 YHJ] 1. Bronze_9316

---
+++ src/com/solved/yuk/etc/bronze/Bronze_9316.java
@@ -0,0 +1,14 @@
+package com.solved.yuk.etc.bronze;
+
+import java.util.Scanner;
+
+public class Bronze_9316 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        for(int i=1; i<=num; i++){
+            System.out.println("Hello World, Judge "+i+"!");
+        }
+    }
+}
