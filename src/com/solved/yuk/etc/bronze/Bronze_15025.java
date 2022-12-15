307e47506aaebab126b73ae02b67bdaa46f296c4
육현진 <37572364+Yukhyeonjin@users.noreply.github.com>
2022-12-15 18:02:43 +0900

    [2022.12.15 YHJ]
    1. Bronze_15025

---
+++ src/com/solved/yuk/etc/bronze/Bronze_15025.java
@@ -0,0 +1,22 @@
+package com.solved.yuk.etc.bronze;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner kb = new Scanner(System.in);
+		int a = kb.nextInt();
+		int b = kb.nextInt();		
+		kb.close();
+		
+		if( a == 0 && b == 0 )
+			System.out.println( "Not a moose" );
+		else if( a == b )
+			System.out.println( "Even " + a*2 );
+		else 
+			System.out.println( "Odd " + Math.max( a, b )*2 );		
+	}
+}
+
