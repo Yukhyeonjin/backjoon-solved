f95460fc840352e5c4d33cb15a58eed5bbff9328
육현진 <37572364+Yukhyeonjin@users.noreply.github.com>
2023-01-21 14:45:49 +0900

    [2023.01.21 YHJ]
    1. Bronze_5691

---
+++ src/com/solved/yuk/etc/bronze/Bronze_5691.java
@@ -0,0 +1,26 @@
+package com.solved.yuk.etc.bronze;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Bronze_5691 {
+    private void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            if (a==0 && b==0) break;
+            int hi = a>b?a:b;
+            int lo = a>b?b:a;
+            sb.append(a-b+a).append('\n');
+        }
+        System.out.print(sb);
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+}
