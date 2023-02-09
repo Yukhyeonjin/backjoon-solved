c430be054afe14cf2f5ef692d3b39ccd461516e1
yughyeonjin <6yj6yj36@naver.com>
2023-02-09 21:52:01 +0900

    [2023.02.09 YHJ] 1. Bronze_5692

---
+++ src/com/solved/yuk/etc/bronze/Bronze_5692.java
@@ -0,0 +1,32 @@
+package com.solved.yuk.etc.bronze;
+
+import java.io.*;
+
+public class Bronze_5692 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        while (true){
+            int number = Integer.parseInt(br.readLine());
+            int FBnum = 0;
+            int count = 0;
+
+            if(number == 0) break;
+            for (int i = number ; i > 0; i /= 10) {
+                int num = i % 10;
+                FBnum += num * factoril(++count);
+            }
+            bw.write(FBnum+"\n");
+        }
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+
+    public static int factoril(int count) {
+        if(count == 1) return 1;
+        return count * factoril(count - 1);
+    }
+}
