73cd26b35641cf1a73ca5a6a5d6fbd8ae29e0da5
Yukhyeonjin <37572364+Yukhyeonjin@users.noreply.github.com>
2023-04-24 23:19:41 +0900

    [2023.04.24 YHJ] 1. Unknown_15890

---
+++ src/com/solved/yuk/etc/unknown/Unknown_15890.java
@@ -0,0 +1,24 @@
+package com.solved.yuk.etc.unknown;
+
+import java.io.*;
+
+public class Unknown_15890 {
+
+    public static void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("20180729");
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        solution();
+    }
+
+
+}
+
