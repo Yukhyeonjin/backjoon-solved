f3a31c454aa9f7c8a5639449f25fab8ea852ee2a
육현진 <37572364+Yukhyeonjin@users.noreply.github.com>
2023-06-17 20:14:58 +0900

    [2023.06.17 YHJ]
    1. Bronze_23806

---
+++ src/com/solved/yuk/etc/bronze/Bronze_23806.java
@@ -0,0 +1,40 @@
+package com.solved.yuk.etc.bronze;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Bronze_23806 {
+    private void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < 5*n; j++) {
+                sb.append('@');
+            }
+            sb.append('\n');
+        }
+        for (int i = 0; i < 3*n; i++) {
+            for (int j = 0; j < n; j++) {
+                sb.append('@');
+            }
+            for (int j = 0; j < 3*n; j++) {
+                sb.append(' ');
+            }
+            for (int j = 0; j < n; j++) {
+                sb.append('@');
+            }
+            sb.append('\n');
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < 5*n; j++) {
+                sb.append('@');
+            }
+            sb.append('\n');
+        }
+        System.out.println(sb);
+    }
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+}
