99b9820cc8b30d44dd3578cf2afa3771a4c564d4
육현진 <37572364+Yukhyeonjin@users.noreply.github.com>
2023-07-07 16:40:04 +0900

    [2023.07.07 YHJ]
    1. Bronze_23808

---
+++ src/com/solved/yuk/etc/bronze/Bronze_23808.java
@@ -0,0 +1,34 @@
+package com.solved.yuk.etc.bronze;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Bronze_23808 {
+    StringBuilder sb = new StringBuilder();
+
+    private void printSplitShells(int n) {
+        for (int j = 0; j < n; j++) sb.append('@');
+        for (int j = 0; j < n*3; j++) sb.append(' ');
+        for (int j = 0; j < n; j++) sb.append('@');
+        sb.append('\n');
+    }
+
+    private void printFullShells(int n) {
+        for (int j = 0; j < n*5; j++) sb.append('@');
+        sb.append('\n');
+    }
+
+    private void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n*2; i++)   printSplitShells(n);
+        for (int i = 0; i < n; i++)     printFullShells(n);
+        for (int i = 0; i < n; i++)     printSplitShells(n);
+        for (int i = 0; i < n; i++)     printFullShells(n);
+        System.out.print(sb);
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Bronze_23808().solution();
+    }
+}
