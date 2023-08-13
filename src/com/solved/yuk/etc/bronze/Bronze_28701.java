ad5211a5a2b5a4ccb202d59f21fa3181eea00e51
Yukhyeonjin <37572364+Yukhyeonjin@users.noreply.github.com>
2023-08-13 21:51:30 +0900

    [2023.08.13 YHJ] 1. Bronze_28701

---
+++ src/com/solved/yuk/etc/bronze/Bronze_28701.java
@@ -0,0 +1,29 @@
+package com.solved.yuk.etc.bronze;
+
+import java.io.*;
+
+public class Bronze_28701 {
+
+    public static void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine()),
+            sum = 0;
+        for(int i = 1; i <= N; i ++){
+            sum += i;
+        }
+        bw.write(sum+"\n"+(sum*sum)+"\n"+(sum*sum));
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        solution();
+    }
+
+
+}
+
