8ecc8ce091d0a35711c3265b527717cb4c59144e
육현진 <37572364+Yukhyeonjin@users.noreply.github.com>
2023-10-08 16:53:24 +0900

    [2023.10.08 YHJ]
    1. Bronze_22864

---
+++ src/com/solved/yuk/etc/bronze/Bronze_22864.java
@@ -0,0 +1,29 @@
+package com.solved.yuk.etc.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Bronze_22864 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken()), c = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		int count = 0, p = 0, answer = 0;
+		while(count != 24) {
+			if(p + a <= m) {
+				answer += b;
+				p += a;
+			}
+			else {
+				p = p - c < 0 ? 0 : p - c;
+			}
+			count++;
+		}
+		System.out.print(answer);
+	}
+}
