499ff2f407a93e0ed9fda29080c48bb1cb95bbd8
Yukhyeonjin <37572364+Yukhyeonjin@users.noreply.github.com>
2023-11-26 20:27:06 +0900

    [2023.11.26 YHJ] 1. Bronze_3765

---
+++ src/com/solved/yuk/etc/bronze/Bronze_3765.java
@@ -0,0 +1,24 @@
+package com.solved.yuk.etc.bronze;
+
+import java.io.*;
+
+public class Bronze_3765 {
+    static void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String input;
+        while ((input = br.readLine()) != null){
+            bw.write(input);
+            bw.newLine();
+        }
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+
+    public static void main(String[] args) throws Exception {
+        solution();
+    }
+}
