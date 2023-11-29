1182c1cf8e638d87a94464696dc35293035056e1
Yukhyeonjin <37572364+Yukhyeonjin@users.noreply.github.com>
2023-11-29 21:21:47 +0900

    [2023.11.29 YHJ] 1. Silver_1343 - 불필요 소스 제거

--- src/com/solved/yuk/etc/silver/Silver_1343.java
+++ src/com/solved/yuk/etc/silver/Silver_1343.java
@@ -10,13 +10,13 @@ public class Silver_1343 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String line = br.readLine()
-               , input[] = line.split("\\.");
-        StringBuffer sb = new StringBuffer();
+        String line = br.readLine();
+        String[] input = line.split("\\.");
+        StringBuilder sb = new StringBuilder();
         boolean flag = true;
         for(int i = 0; i < input.length; i ++){
             int length = input[i].length();
-            if(length != 0 && length % 4 != 0 && length % 2 != 0) {
+            if(length % 2 != 0) {
                 flag = false;
                 break;
             }
@@ -24,7 +24,6 @@ public class Silver_1343 {
                 if(length - j >= 4 && "XXXX".equals(input[i].substring(j,j+4))){
                     sb.append("AAAA");
                     j+=4;
-                    continue;
                 }
                 else{
                     sb.append("BB");
@@ -34,13 +33,12 @@ public class Silver_1343 {
             if(i != input.length - 1)sb.append(".");
         }
         if(flag) {
-            String result = sb.toString();
+            StringBuilder result = new StringBuilder(sb.toString());
             int resultLength = result.length();
-            if(result.length() != line.length());
             for(int i = 0; i < line.length() - resultLength; i ++){
-                result+=".";
+                result.append(".");
             }
-            bw.write(result);
+            bw.write(result.toString());
         }
         else bw.write("-1");
 
