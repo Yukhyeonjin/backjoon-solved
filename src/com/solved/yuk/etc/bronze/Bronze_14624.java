bf95752d39a099a420c13743040d578b7a52f4db
육현진 <37572364+Yukhyeonjin@users.noreply.github.com>
2024-02-04 21:58:11 +0700

    Create Bronze_14624.java

---
+++ src/com/solved/yuk/etc/bronze/Bronze_14624.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+ 
+public class question_14624 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        sc.close();
+ 
+        if (a % 2 == 0) {
+            System.out.println("I LOVE CBNU");
+        } else {
+            for (int i = 0; i < a; i++) System.out.printf("*");
+            System.out.printf("\n");
+            int c = Math.round(a / 2) + 1;
+            for (int i = 0; i < c; i++) {
+                if (i == 0) System.out.println(getSpace(a / 2) + "*");
+                else {
+                    int center = (i == 1) ? i : i * 2 - 1;
+                    System.out.println(getSpace(a / 2 - i) + "*" + getSpace(center) + "*" );
+                }
+            }
+        }
+    }
+ 
+    public static String getSpace(int count) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < count; i++) sb.append(" ");
+        return sb.toString();
+    }
+}
