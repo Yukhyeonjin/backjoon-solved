86f76506c83907e71d9c6da86aa57c5e2414e839
육현진 <37572364+Yukhyeonjin@users.noreply.github.com>
2024-02-05 21:09:07 +0700

    [2024.02.05 YHJ]
    1. Bronze_14405

---
+++ src/com/solved/yuk/etc/bronze/Bronze_14405.java
@@ -0,0 +1,28 @@
+package com.solved.yuk.etc.bronze;
+
+import java.util.Scanner;
+
+public class Bronze_14405 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int cnt = 0;
+		String input = sc.next();
+		
+		input = input.replace("pi","*");
+		input = input.replace("ka","*");
+		input = input.replace("chu","*");
+		
+		
+		for (int i = 0; i < input.length(); i++) {
+			if(input.charAt(i)=='*') {
+				cnt++;
+			}
+		}
+		
+		if(input.length()==cnt) {
+			System.out.println("YES");
+		} else {
+			System.out.println("NO");
+		}
+	}
+}
