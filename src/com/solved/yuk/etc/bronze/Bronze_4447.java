723bfaa1a3895db6189748cd97118b6aedc9186b
육현진 <37572364+Yukhyeonjin@users.noreply.github.com>
2024-02-06 20:20:39 +0700

    [2024.02.06 YHJ]
    1. Bronze_4447

---
+++ src/com/solved/yuk/etc/bronze/Bronze_4447.java
@@ -0,0 +1,35 @@
+package com.solved.yuk.etc.bronze;
+
+import java.util.Scanner;
+
+public class Bronze_4447 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		int N = Integer.parseInt(scan.nextLine());
+		for (int i = 0; i < N; i++) {
+			String str = scan.nextLine();
+			int g = 0;
+			int b = 0;
+			String dummy = str.replace(" ", "").toLowerCase();
+			for (int j = 0; j < dummy.length(); j++) {
+				if (dummy.charAt(j) == 'g')
+					++g;
+				else if (dummy.charAt(j) == 'b')
+					++b;
+			}
+			String ret = " is ";
+			if (g > b)
+				ret += "GOOD";
+			else if (g < b)
+				ret += "A BADDY";
+			else
+				ret += "NEUTRAL";
+
+			System.out.println(str + ret);
+		}
+
+		scan.close();
+	}
+}
