5a79d03e667f27311f5bb870780c23ea233587ce
육현진 <37572364+Yukhyeonjin@users.noreply.github.com>
2024-04-08 17:21:31 +0900

    [2024.04.08 YHJ]
    1. Bronze_12780

---
+++ src/com/solved/yuk/etc/bronze/Bronze_12780.java
@@ -0,0 +1,21 @@
+package com.solved.yuk.etc.bronze;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Bronze_12780 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String a = br.readLine();
+        String b = br.readLine();
+        int idx = -1;
+        int cnt = 0;
+        while (idx != 0 && idx <= a.length()) {
+            idx = a.indexOf(b, idx);
+            if (idx != -1)
+                cnt++;
+            idx++;
+        }
+        System.out.println(cnt);
+    }
+}
