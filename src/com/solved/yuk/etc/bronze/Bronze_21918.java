6cbe2bba056ce573f6ce9d1700d6c9c89b2a2006
육현진 <37572364+Yukhyeonjin@users.noreply.github.com>
2024-05-18 12:54:18 +0900

    [2024.05.18 YHJ]
    1. Bronze_21918

---
+++ src/com/solved/yuk/etc/bronze/Bronze_21918.java
@@ -0,0 +1,47 @@
+package com.solved.yuk.etc.bronze;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Bronze_21918 {
+    private void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n+1];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++)
+            arr[i] = Integer.parseInt(st.nextToken());
+
+        while (m-->0) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            switch (a) {
+                case 1 :
+                    arr[b] = c;
+                    break;
+                case 2 :
+                    for (int i = b; i <= c; i++) arr[i]^=1;
+                    break;
+                case 3 :
+                    for (int i = b; i <= c; i++) arr[i] = 0;
+                    break;
+                case 4 :
+                    for (int i = b; i <= c; i++) arr[i] = 1;
+                    break;
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++)
+            sb.append(arr[i]).append(' ');
+        System.out.println(sb);
+    }
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+}
