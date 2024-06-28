bb6982f717b330600193599a2e85e8fe9b095920
육현진 <37572364+Yukhyeonjin@users.noreply.github.com>
2024-06-28 17:18:58 +0900

    [2024.06.28 YHJ]
    1. Bronze_1809

---
+++ src/com/solved/yuk/etc/bronze/Bronze_1809.java
@@ -0,0 +1,20 @@
+package com.solved.yuk.etc.bronze;
+
+import java.io.*;
+
+public class Bronze_1809 {
+    static void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("(___)\n(o o)____/\n@@      \\\n\\ ____,/\n//   //\n^^   ^^\n");
+        
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+
+    public static void main(String[] args) throws Exception {
+        solution();
+    }
+}
