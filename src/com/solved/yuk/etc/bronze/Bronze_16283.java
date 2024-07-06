80808672b424c0ebb606aac3cf1ce38daca72766
육현진 <37572364+Yukhyeonjin@users.noreply.github.com>
2024-07-06 23:13:26 +0900

    [2024.07.06 YHJ]
    1. Bronze_16283

---
+++ src/com/solved/yuk/etc/bronze/Bronze_16283.java
@@ -0,0 +1,32 @@
+package com.solved.yuk.bronze;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Bronze_16283 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int a = read(), b = read(), n = read(), w = read(), m = -1;
+        for (int i = 1; i < n; i++) {
+            if (i * a + (n - i) * b == w) {
+                if (m != -1) {
+                    m = -1;
+                    break;
+                } else m = i;
+            }
+        }
+
+        bw.write(m != -1 ? m + " " + (n - m) : "-1");
+        bw.flush();
+    }
+
+    private static int read() throws IOException {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+
+        return n;
+    }
+
