950fb48f68b92bdde9443f9875293e5fddd047c0
육현진 <6yj6yj36@naver.com>
2024-09-16 15:50:21 +0900

    [2024.09.16 YHJ] 1. Bronze_32314 update

--- src/com/solved/yuk/etc/bronze/Bronze_32314.java
+++ src/com/solved/yuk/etc/bronze/Bronze_32314.java
@@ -11,7 +11,7 @@ public class Bronze_32314 {
         String[] input = br.readLine().split(" ");
         int w = Integer.parseInt(input[0]);
         int v = Integer.parseInt(input[1]);
-        if(w == a*v && a == w / v) bw.write("1");
+        if(w >= a*v) bw.write("1");
         else bw.write("0");
 
         bw.flush();
