b76d4ee02a37636e1aeeaba006bb6bd1f5b3008a
육현진 <6yj6yj36@naver.com>
2024-09-16 15:46:45 +0900

    1. Bronze_32314

---
+++ src/com/solved/yuk/etc/bronze/Bronze_32314.java
@@ -0,0 +1,25 @@
+package com.solved.yuk.etc.bronze;
+
+import java.io.*;
+
+public class Bronze_32314 {
+    static void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int a = Integer.parseInt(br.readLine());
+        String[] input = br.readLine().split(" ");
+        int w = Integer.parseInt(input[0]);
+        int v = Integer.parseInt(input[1]);
+        if(w == a*v && a == w / v) bw.write("1");
+        else bw.write("0");
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+    
+    public static void main(String[] args) throws Exception {
+        solution();
+    }
+}
